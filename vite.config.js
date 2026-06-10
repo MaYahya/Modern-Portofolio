5eb92bfe559c446e343d3ba58c09448d8054d82a
MA Yahya <ayyas.adam@gmail.com>
2026-06-10 23:49:10 +0300

    Update location and fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/yahya_ayz/',
+  base: '/Modern-Portofolio/',
   plugins: [react(), tailwindcss()],
 })
