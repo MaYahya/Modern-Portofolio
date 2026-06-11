5b31aeb743da00b3b3123ec69e86e71bf7e1b291
MA Yahya <ayyas.adam@gmail.com>
2026-06-11 12:34:59 +0300

    Restore all sections to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,17 @@
 import Hero from '../components/Hero'
+import About from '../components/About'
+import Skills from '../components/Skills'
+import Contact from '../components/Contact'
 
 const HomePage = () => {
-  return <Hero />
+  return (
+    <>
+      <Hero />
+      <About />
+      <Skills />
+      <Contact />
+    </>
+  )
 }
 
 export default HomePage
